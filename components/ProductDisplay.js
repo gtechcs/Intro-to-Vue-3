9523c39f8ea0a5873cb4694a0f500a6391c37ad7
Puneet Kumar <puneet.kumar@walmart.com>
2021-04-11 16:43:56 -0700

    L10

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,13 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="removeFromCart">
+          Remove from Cart
+        </button>
       </div>
     </div>
   </div>`,
@@ -56,9 +63,14 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          // this.cart += 1;
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id);
       },
-      updateVariant(index) {
+      removeFromCart() {
+        // this.cart += 1;
+        this.$emit('remove-from-cart', this.variants[this.selectedVariant].id);
+    },
+    updateVariant(index) {
           this.selectedVariant = index
       }
   },
